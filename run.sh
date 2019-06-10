60c0a88000fa8da9f282d8c7e8927ba81a653191
Sébastien HUSS <sebastien.huss@gmail.com>
2019-06-10 09:46:08 +0200

    Test

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 #  and https://github.com/komljen/drone-kubectl-helm/blob/master/run.sh
 
 kubectl config set-credentials default --token=${PLUGIN_TOKEN}
-if [[ ! -z ${PLUGIN_SERVER} ]]; then
-  PLUGIN_SERVER=https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS}
+if [[ -z ${PLUGIN_SERVER} ]]; then
+  PLUGIN_SERVER="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS}"
 fi
 if [[ ! -z ${PLUGIN_CERT} ]]; then
   echo ${PLUGIN_CERT} | base64 -d >ca.crt
@@ -13,10 +13,6 @@ else
   echo "WARNING: Using insecure connection to cluster"
   kubectl config set-cluster default --server=${PLUGIN_SERVER} --insecure-skip-tls-verify=true
 fi
-
-echo "====================================="
-find /run/secrets
-echo "====================================="
 
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
