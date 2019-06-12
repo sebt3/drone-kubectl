287b756c4020999ca50ce0a380e27c107cd8c25b
Sébastien HUSS <sebastien.huss@gmail.com>
2019-06-12 19:40:38 +0200

    Added patch deployement capability

--- README.md
+++ README.md
@@ -43,3 +43,42 @@ get:
 ```
 
 The setting server is optional.
+
+Use this to patch the image in a deployment :
+```
+kind: pipeline
+name: default
+steps:
+- name: kubectl
+  image: <your repo here>/drone-kubectl
+  settings:
+    cert:
+      from_secret: kubernetes_cert
+    token:
+      from_secret: kubernetes_token
+    patch_deploy: my_deploy
+    namespace: toolchain
+    container: my_container
+    registry: docker.io
+    repo: sebt3/my_image
+    tag: 1.2.3
+---
+kind: secret
+name: kubernetes_cert
+get:
+  path: drone-kubernetes
+  name: cert
+---
+kind: secret
+name: kubernetes_token
+get:
+  path: drone-kubernetes
+  name: token
+```
+
+- patch_deploy: the deployment to patch
+- namespace: default "default"
+- container: the container in the pods to patch, default to the deployment name
+- registry: the registry the image will be pulled from (can be omitted)
+- repo: The image name, default to ${DRONE_REPO_NAME}
+- tag: the image tag to use, by default will use the 1st tag in the .tags file, use "latest" if not found
--- run.sh
+++ run.sh
@@ -2,38 +2,60 @@
 # Inspired by https://github.com/honestbee/drone-kubernetes/blob/master/update.sh
 #  and https://github.com/komljen/drone-kubectl-helm/blob/master/run.sh
 
+### Init phase
 kubectl config set-credentials default --token=${PLUGIN_TOKEN}
 if [[ -z ${PLUGIN_SERVER} ]]; then
-  PLUGIN_SERVER="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS}"
+	PLUGIN_SERVER="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS}"
 fi
 if [[ ! -z ${PLUGIN_CERT} ]]; then
-  echo ${PLUGIN_CERT} | base64 -d >ca.crt
-  kubectl config set-cluster default --server=${PLUGIN_SERVER} --certificate-authority=ca.crt
+	echo ${PLUGIN_CERT} | base64 -d >ca.crt
+	kubectl config set-cluster default --server=${PLUGIN_SERVER} --certificate-authority=ca.crt
 else
-  echo "WARNING: Using insecure connection to cluster"
-  kubectl config set-cluster default --server=${PLUGIN_SERVER} --insecure-skip-tls-verify=true
+	echo "WARNING: Using insecure connection to cluster"
+	kubectl config set-cluster default --server=${PLUGIN_SERVER} --insecure-skip-tls-verify=true
 fi
 
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
-echo "====================================="
-pwd
-echo "====================================="
-find
+
+### Patching deployment
+if [[ ! -z ${PLUGIN_PATCH_DEPLOY} ]]; then
+	if [[ ! -z ${PLUGIN_TAG} ]]; then
+		tag=${PLUGIN_TAG}
+	elif [ -f ".tags" ];then
+		tag=$(sed 's/,.*//' .tags)
+	else
+		echo "Warning: no tags set and no .tags file available, using 'latest'"
+		tag="latest"
+	fi
+	image="${PLUGIN_REGISTRY}/${PLUGIN_REPO:-"${DRONE_REPO_NAME}"}:$tag"
+	image="${image#/}"
+fi
+if [[ ! -z ${PLUGIN_PATCH_DEPLOY} ]]; then
+	echo "${PLUGIN_PATCH_DEPLOY}"|sed 's/,/\n/g'|while read deploy;do 
+		name="${PLUGIN_CONTAINER:-"${deploy}"}"
+		echo ${PLUGIN_NAMESPACE:-"default"}|while read ns;do 
+			echo "====================================="
+			echo "Patching deployement $deploy in namespace $ns to use $image as image for $name container"
+			kubectl patch deploy -n "$ns" "${deploy}" --patch '{"spec": {"template": {"spec": {"containers": [{"image": "'"$image"'","name": "'"$name"'"}]}}}}'
+		done
+	done
+fi
+
 # Run kubectl command
 if [[ ! -z ${PLUGIN_KUBECTL} ]]; then
-  echo "${PLUGIN_KUBECTL}"|sed 's/,/\n/g'|while read CMD;do 
-    echo "====================================="
-    echo "# kubectl ${CMD}"
-    kubectl ${CMD}
-  done
+	echo "${PLUGIN_KUBECTL}"|sed 's/,/\n/g'|while read CMD;do 
+		echo "====================================="
+		echo "# kubectl ${CMD}"
+		kubectl ${CMD}
+	done
 fi
 
 # Run helm command
 if [[ ! -z ${PLUGIN_HELM} ]]; then
-  echo "${PLUGIN_HELM}"|sed 's/,/\n/g'|while read CMD;do 
-    echo "====================================="
-    echo "# helm ${CMD}"
-    helm ${CMD}
-  done
+	echo "${PLUGIN_HELM}"|sed 's/,/\n/g'|while read CMD;do 
+		echo "====================================="
+		echo "# helm ${CMD}"
+		helm ${CMD}
+	done
 fi
