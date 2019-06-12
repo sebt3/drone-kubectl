e3563bed63d2a2a7ca9ba6c73825faa17e766446
Sébastien HUSS <sebastien.huss@gmail.com>
2019-06-12 21:00:40 +0200

    Add statefulset, daemonset patching capability and helm upgrade

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![Build Status](http://drone.cloud.infra.local/api/badges/seb/drone-kubectl/status.svg)](http://drone.cloud.infra.local/seb/drone-kubectl)
 
 ## Plugin for drone.io
-Usage :
+### Basic usage
 ```
 kind: pipeline
 name: default
@@ -44,7 +44,8 @@ get:
 
 The setting server is optional.
 
-Use this to patch the image in a deployment :
+
+### Patch a deployment
 ```
 kind: pipeline
 name: default
@@ -82,3 +83,45 @@ get:
 - registry: the registry the image will be pulled from (can be omitted)
 - repo: The image name, default to ${DRONE_REPO_NAME}
 - tag: the image tag to use, by default will use the 1st tag in the .tags file, use "latest" if not found
+
+### Upgrade an helm install
+```
+kind: pipeline
+name: default
+steps:
+- name: kubectl
+  image: <your repo here>/drone-kubectl
+  settings:
+    cert:
+      from_secret: kubernetes_cert
+    token:
+      from_secret: kubernetes_token
+    upgrade_helm: gogs
+    source: incubator/gogs
+    tag: 1.2.3
+    tag_value: image.tag
+    registry: docker.io
+    repo: sebt3/my_image
+    image_value: image.repo
+---
+kind: secret
+name: kubernetes_cert
+get:
+  path: drone-kubernetes
+  name: cert
+---
+kind: secret
+name: kubernetes_token
+get:
+  path: drone-kubernetes
+  name: token
+```
+
+- upgrade_helm: the name of the helm install **required**
+- source: source for the helm chart **required**
+- image_value: setting name for the image to be changed using the value of the following 2
+- registry: the registry the image will be pulled from (can be omitted)
+- repo: The image name, default to ${DRONE_REPO_NAME}
+- tag_value: setting name for the tag to be changed using the value of the following
+- tag: the image tag to use, by default will use the 1st tag in the .tags file, use "latest" if not found
+Either image_value or tag_value have to be set
--- run.sh
+++ run.sh
@@ -18,8 +18,11 @@ fi
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
 
-### Patching deployment
-if [[ ! -z ${PLUGIN_PATCH_DEPLOY} ]]; then
+RC=0
+DONE=0
+
+### Defining variable for patches
+if [[ ! -z ${PLUGIN_PATCH_DEPLOY} ]] || [[ ! -z ${PLUGIN_UPGRADE_HELM} ]]; then
 	if [[ ! -z ${PLUGIN_TAG} ]]; then
 		tag=${PLUGIN_TAG}
 	elif [ -f ".tags" ];then
@@ -31,31 +34,109 @@ if [[ ! -z ${PLUGIN_PATCH_DEPLOY} ]]; then
 	image="${PLUGIN_REGISTRY}/${PLUGIN_REPO:-"${DRONE_REPO_NAME}"}:$tag"
 	image="${image#/}"
 fi
-if [[ ! -z ${PLUGIN_PATCH_DEPLOY} ]]; then
-	echo "${PLUGIN_PATCH_DEPLOY}"|sed 's/,/\n/g'|while read deploy;do 
-		name="${PLUGIN_CONTAINER:-"${deploy}"}"
-		echo ${PLUGIN_NAMESPACE:-"default"}|while read ns;do 
-			echo "====================================="
-			echo "Patching deployement $deploy in namespace $ns to use $image as image for $name container"
-			kubectl patch deploy -n "$ns" "${deploy}" --patch '{"spec": {"template": {"spec": {"containers": [{"image": "'"$image"'","name": "'"$name"'"}]}}}}'
-		done
-	done
-fi
 
-# Run kubectl command
-if [[ ! -z ${PLUGIN_KUBECTL} ]]; then
-	echo "${PLUGIN_KUBECTL}"|sed 's/,/\n/g'|while read CMD;do 
+
+### Run helm upgrade
+if [[ ! -z ${PLUGIN_UPGRADE_HELM} ]] &&  [[ ! -z ${PLUGIN_SOURCE} ]]; then
+	CMD=""
+	if [[ ! -z ${PLUGIN_TAG_VALUE} ]];then
+		CMD=" --set ${PLUGIN_TAG_VALUE}=$tag"
+	fi
+	if [[ ! -z ${PLUGIN_IMAGE_VALUE} ]];then
+		CMD=" --set ${PLUGIN_IMAGE_VALUE}=$image"
+	fi
+	if [[ ! -z ${CMD} ]];then
 		echo "====================================="
-		echo "# kubectl ${CMD}"
-		kubectl ${CMD}
-	done
+		echo "#helm upgrade --reuse-values $CMD ${PLUGIN_UPGRADE_HELM} ${PLUGIN_SOURCE}"
+		if ! helm upgrade --reuse-values $CMD ${PLUGIN_UPGRADE_HELM} ${PLUGIN_SOURCE};then
+			echo "helm upgrade FAILED!"
+			RC=$(($RC+1))
+		fi
+	else
+		echo "ERROR: both 'upgrade_helm' and 'source' settings are required"
+		RC=$(($RC+1))
+	fi
+elif [[ ! -z ${PLUGIN_UPGRADE_HELM} ]] ||  [[ ! -z ${PLUGIN_SOURCE} ]];then
+	echo "ERROR: both 'upgrade_helm' and 'source' settings are required"
+	RC=$(($RC+1))
 fi
 
-# Run helm command
+### Run helm command
 if [[ ! -z ${PLUGIN_HELM} ]]; then
 	echo "${PLUGIN_HELM}"|sed 's/,/\n/g'|while read CMD;do 
 		echo "====================================="
 		echo "# helm ${CMD}"
-		helm ${CMD}
+		if ! helm ${CMD};then
+			echo "helm FAILED!"
+			RC=$(($RC+1))
+		fi
 	done
 fi
+
+### Patching deployment
+if [[ ! -z ${PLUGIN_PATCH_DEPLOY} ]]; then
+	DONE=1
+	echo "${PLUGIN_PATCH_DEPLOY}"|sed 's/,/\n/g'|while read deploy;do 
+		name="${PLUGIN_CONTAINER:-"${deploy}"}"
+		echo ${PLUGIN_NAMESPACE:-"default"}|while read ns;do 
+			echo "====================================="
+			echo "Patching deployment $deploy in namespace $ns to use $image as image for $name container"
+			
+			if ! kubectl patch deploy -n "$ns" "${deploy}" --patch '{"spec": {"template": {"spec": {"containers": [{"image": "'"$image"'","name": "'"$name"'"}]}}}}';then
+				echo "Patching FAILED !"
+				RC=$(($RC+1))
+			fi
+		done
+	done
+fi
+### Patching statefulset
+if [[ ! -z ${PLUGIN_PATCH_STATEFULSET} ]]; then
+	DONE=1
+	echo "${PLUGIN_PATCH_STATEFULSET}"|sed 's/,/\n/g'|while read deploy;do 
+		name="${PLUGIN_CONTAINER:-"${deploy}"}"
+		echo ${PLUGIN_NAMESPACE:-"default"}|while read ns;do 
+			echo "====================================="
+			echo "Patching statefulset $deploy in namespace $ns to use $image as image for $name container"
+			
+			if ! kubectl patch statefulset -n "$ns" "${deploy}" --patch '{"spec": {"template": {"spec": {"containers": [{"image": "'"$image"'","name": "'"$name"'"}]}}}}';then
+				echo "Patching FAILED !"
+				RC=$(($RC+1))
+			fi
+		done
+	done
+fi
+### Patching daemonset
+if [[ ! -z ${PLUGIN_PATCH_DAEMONSET} ]]; then
+	DONE=1
+	echo "${PLUGIN_PATCH_DAEMONSET}"|sed 's/,/\n/g'|while read deploy;do 
+		name="${PLUGIN_CONTAINER:-"${deploy}"}"
+		echo ${PLUGIN_NAMESPACE:-"default"}|while read ns;do 
+			echo "====================================="
+			echo "Patching daemonset $deploy in namespace $ns to use $image as image for $name container"
+			
+			if ! kubectl patch daemonset -n "$ns" "${deploy}" --patch '{"spec": {"template": {"spec": {"containers": [{"image": "'"$image"'","name": "'"$name"'"}]}}}}';then
+				echo "Patching FAILED !"
+				RC=$(($RC+1))
+			fi
+		done
+	done
+fi
+
+### Run kubectl command
+if [[ ! -z ${PLUGIN_KUBECTL} ]]; then
+	DONE=1
+	echo "${PLUGIN_KUBECTL}"|sed 's/,/\n/g'|while read CMD;do 
+		echo "====================================="
+		echo "# kubectl ${CMD}"
+		if ! kubectl ${CMD};then
+			echo "kubectl FAILED!"
+			RC=$(($RC+1))
+		fi
+	done
+fi
+
+if [ $DONE -eq 0 ];then
+	echo "The plugin did nothing"
+	exit 1
+fi
+exit $RC
